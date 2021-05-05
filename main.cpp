cf5a615624b538cb997d57901b6bb3dd165ca415
GRosenzweig <rosenzweiggila@gmail.com>
2021-05-04 21:20:19 -0400

    submission

--- main.cpp
+++ main.cpp
@@ -18,60 +18,60 @@ using namespace std;
 // A simple class; each object holds four public fields
 class Data {
 public:
-    string lastName;
-    string firstName;
-    string ssn;
+  string lastName;
+  string firstName;
+  string ssn;
 };
 
 // Load the data from a specified input file
 void loadDataList(list<Data *> &l, const string &filename) {
 
-    ifstream input(filename);
-    if (!input) {
-        cerr << "Error: could not open " << filename << "\n";
-        exit(1);
-    }
+  ifstream input(filename);
+  if (!input) {
+    cerr << "Error: could not open " << filename << "\n";
+    exit(1);
+  }
 
-    // The first line indicates the size
-    string line;
+  // The first line indicates the size
+  string line;
+  getline(input, line);
+  stringstream ss(line);
+  int size;
+  ss >> size;
+
+  // Load the data
+  for (int i = 0; i < size; i++) {
     getline(input, line);
-    stringstream ss(line);
-    int size;
-    ss >> size;
+    stringstream ss2(line);
+    Data *pData = new Data();
+    ss2 >> pData->lastName >> pData->firstName >> pData->ssn;
+    l.push_back(pData);
+  }
 
-    // Load the data
-    for (int i = 0; i < size; i++) {
-        getline(input, line);
-        stringstream ss2(line);
-        Data *pData = new Data();
-        ss2 >> pData->lastName >> pData->firstName >> pData->ssn;
-        l.push_back(pData);
-    }
-
-    input.close();
+  input.close();
 }
 
 // Output the data to a specified output file
 void writeDataList(const list<Data *> &l, const string &filename) {
 
-    ofstream output(filename);
-    if (!output) {
-        cerr << "Error: could not open " << filename << "\n";
-        exit(1);
-    }
+  ofstream output(filename);
+  if (!output) {
+    cerr << "Error: could not open " << filename << "\n";
+    exit(1);
+  }
 
-    // Write the size first
-    int size = l.size();
-    output << size << "\n";
+  // Write the size first
+  int size = l.size();
+  output << size << "\n";
 
-    // Write the data
-    for (auto pData:l) {
-        output << pData->lastName << " "
-               << pData->firstName << " "
-               << pData->ssn << "\n";
-    }
+  // Write the data
+  for (auto pData:l) {
+    output << pData->lastName << " "
+       << pData->firstName << " "
+       << pData->ssn << "\n";
+  }
 
-    output.close();
+  output.close();
 }
 
 // Sort the data according to a specified field
@@ -81,27 +81,27 @@ void sortDataList(list<Data *> &);
 // The main function calls routines to get the data, sort the data,
 // and output the data. The sort is timed according to CPU time.
 int main() {
-    string filename;
-    cout << "Enter name of input file: ";
-    cin >> filename;
-    list<Data *> theList;
-    loadDataList(theList, filename);
+  string filename;
+  cout << "Enter name of input file: ";
+  cin >> filename;
+  list<Data *> theList;
+  loadDataList(theList, filename);
 
-    cout << "Data loaded.\n";
+  cout << "Data loaded.\n";
 
-    cout << "Executing sort...\n";
-    clock_t t1 = clock();
-    sortDataList(theList);
-    clock_t t2 = clock();
-    double timeDiff = ((double) (t2 - t1)) / CLOCKS_PER_SEC;
+  cout << "Executing sort...\n";
+  clock_t t1 = clock();
+  sortDataList(theList);
+  clock_t t2 = clock();
+  double timeDiff = ((double) (t2 - t1)) / CLOCKS_PER_SEC;
 
-    cout << "Sort finished. CPU time was " << timeDiff << " seconds.\n";
+  cout << "Sort finished. CPU time was " << timeDiff << " seconds.\n";
 
-    cout << "Enter name of output file: ";
-    cin >> filename;
-    writeDataList(theList, filename);
+  cout << "Enter name of output file: ";
+  cin >> filename;
+  writeDataList(theList, filename);
 
-    return 0;
+  return 0;
 }
 
 // -------------------------------------------------
