ad343ecfa90d6b5e1ca866c9ab79254a3703e466
GRosenzweig <rosenzweiggila@gmail.com>
2021-05-04 22:03:19 -0400

    submission

--- main.cpp
+++ main.cpp
@@ -111,9 +111,9 @@ int main() {
 // You may add global variables, functions, and/or
 // class definitions here if you wish.
 
-string ssn[1010000];
 
 bool compare(const Data* id1, const Data* id2) {
+    //implementation of the built-in l.sort()
     if (id1->lastName < id2->lastName)
         return true;
     if (id1->lastName > id2->lastName)
@@ -127,21 +127,17 @@ bool compare(const Data* id1, const Data* id2) {
     else
         return false;
 }
-bool ssnCompare(const Data* id1, const Data* id2){
-    if(id1->ssn <= id2->ssn)
-        return true;
-    else
-        return false;
-}
 
-
-// C++ implementation of Radix Sort
+//implementation of radix sort
 int ssnStringToInt(Data* data){
+    //converts SSN string to an int, to be sorted with radix sort
     return stoi(data->ssn.substr(0,3) + data->ssn.substr(4,2) + data->ssn.substr(7,4));
 }
 
 string ssnIntToString(int ssn){
+    //converts ssn in int form to string
     string stringSSN = to_string(ssn);
+    //add leading 0's until the string length is 9
     while(!(stringSSN.length() == 9))
         stringSSN = "0" + stringSSN;
     stringSSN.insert(3, "-");
@@ -150,6 +146,7 @@ string ssnIntToString(int ssn){
 }
 
 int getMax(const int arr[], int len){
+    //finds the max length of the ssn in int form (not all will be 9 digits, b/c conversion drops leading 0's)
     int max = arr[0];
     for (int i = 1; i < len; i++){
         if (arr[i] > max)
@@ -158,48 +155,43 @@ int getMax(const int arr[], int len){
     return max;
 }
 
-int ssnArray[1010001];
+int ssnArray[1010001]; //array to store ssn once converted to int form
 
 void binSort(int preSort[], int len, int position){
     int buckets[10] = {0};
-
-    for(int i = 0; i < len; i++){
+    for(int i = 0; i < len; i++)
         buckets[(preSort[i]/position) % 10]++;
-    }
-    for(int bucketIndex = 1; bucketIndex < 10; bucketIndex++){
+    for(int bucketIndex = 1; bucketIndex < 10; bucketIndex++)
         buckets[bucketIndex] += buckets[bucketIndex - 1];
-    }
     for(int j = len - 1; j >= 0; j--){
         ssnArray[buckets[(preSort[j]/position) % 10] - 1] = preSort[j];
         buckets[(preSort[j]/position) % 10]--;
     }
-    for (int k = 0; k < len; k++){
+    for (int k = 0; k < len; k++)
         preSort[k] = ssnArray[k];
-    }
 }
 
 void radixSort(int preSort[], int len){
     int max = getMax(preSort, len);
-    for (int position = 1; max/position > 0; position *= 10){
+    for (int position = 1; max/position > 0; position *= 10)
         binSort(preSort, len, position);
-    }
 }
 
-int tempArr[1010001];
+int tempArr[1010001]; //array of int versions of ssn to be sorted in radix, allocated enough memory to store within 1% of 1mil ssn
+//made global so it is static memory
 
 void sortDataList(list<Data *> &l) {
     // Fill this in;
+    //for samples 1, 2, 3, use l.sort()
     if (!(l.front()->lastName == l.back()->lastName))
         l.sort(compare);
+    //for sample4 use radix on ssn
     else {
-        int i = 0;
-        //l.sort(ssnCompare);
-        int lSize = l.size();
+        int i = 0, lSize = l.size();
         for(auto ids:l){
             if(i == lSize)
                 break;
-            tempArr[i] = ssnStringToInt(ids);
-            i++;
+            tempArr[i++] = ssnStringToInt(ids);
         }
         radixSort(tempArr, lSize);
 
@@ -207,8 +199,7 @@ void sortDataList(list<Data *> &l) {
         for(auto ids:l){
             if(i == lSize)
                 break;
-            ids->ssn = ssnIntToString(tempArr[i]);
-            i++;
+            ids->ssn = ssnIntToString(tempArr[i++]);
         }
     }
 }
